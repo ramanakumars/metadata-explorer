d018517890511ca0fc5dcbde17824169221e551e
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2024-01-11 01:39:15 -0800

    adding tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '2': 'repeat(2, minmax(0, 1fr))',
+        '4': 'repeat(4, minmax(0, 1fr))',
+        '8': 'repeat(8, minmax(0, 1fr))',
+        '16': 'repeat(16, minmax(0, 1fr))',
+        '32': 'repeat(32, minmax(0, 1fr))',
+      },
+    },
   },
   plugins: [],
 }
